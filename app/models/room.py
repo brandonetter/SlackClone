02efa5dd0bb9fec879fb83f3e9ade62c57851b9d
Brandon EtteR <brandonetter@gmail.com>
2023-03-24 19:45:30 -0400

    t

--- app/models/room.py
+++ app/models/room.py
@@ -12,7 +12,7 @@ class Room(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(40), nullable=False)
     createdby = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
-    type = db.Column(db.String(40), db.ForeignKey(add_prefix_for_prod('types.id')), nullable=False)
+    type = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('types.id')), nullable=False)
     createdat = db.Column(db.DateTime, server_default=func.now(),default=func.now())
     updatedat = db.Column(db.DateTime, onupdate=func.now(),default=func.now())
     members = db.relationship('Room_Member', backref='room', lazy=True)
